1f401f064cd7f1f1bed6045501f4083b61c7eb0f
EswaramoorthyS <smeswaramoorthy@gmail.com>
2023-10-05 22:44:04 +0530

    initial commit

---
+++ api/controllers/test/test.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+    friendlyName: 'Welcome user',
+
+    description: 'Look up the specified user and welcome them, or redirect to a signup page if no user was found.',
+
+    inputs: {
+
+    },
+
+    fn: async function () {
+
+        let val = "hello connected successs"
+        return val;
+       
+    }
+};
